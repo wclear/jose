e45f4c0e7dfd774abc78326bc7b83ca4c3ac04e5
Will <756769+wclear@users.noreply.github.com>
2022-03-18 08:49:53 +0100

    Throw error if timespan is invalid

--- src/lib/validateTimeSpan.ts
+++ src/lib/validateTimeSpan.ts
@@ -1,10 +1,9 @@
 import secs from './secs.js'
 
-export function validateTimeSpan(str: string): boolean {
-  try {
-    const value = secs(str);
-    return typeof value === 'number' && !isNaN(value);
-  } catch (err) {
-    return false;
+export function validateTimeSpan(str: string) {
+  const value = secs(str);
+  const isValidNumber = typeof value === 'number' && !isNaN(value);
+  if (!isValidNumber) {
+    throw new TypeError('Number in time span string must be parseable as a float');
   }
 }
