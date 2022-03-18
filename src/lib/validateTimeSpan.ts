90065aaf75162dee7bc204479ea6edc556830e2a
Will <756769+wclear@users.noreply.github.com>
2022-03-18 08:38:14 +0100

    Update and rename validate_secs.ts to validateTimeSpan.ts

--- src/lib/validateTimeSpan.ts
+++ src/lib/validateTimeSpan.ts
@@ -1,6 +1,6 @@
 import secs from './secs.js'
 
-export default (str: string): boolean => {
+export function validateTimeSpan(str: string): boolean {
   try {
     const value = secs(str);
     return typeof value === 'number' && !isNaN(value);
